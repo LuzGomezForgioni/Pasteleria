085d313e09dff8705c9f816cba23d9cd606db7ab
Luz Gómez Forgioni <luzgomezforgioni@gmail.com>
2022-10-17 18:17:20 -0300

    Actualizacion pagina web

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// MENU
 window.addEventListener("DOMContentLoaded", (event) => {
     const menuBtn = document.getElementById("menu");
     const nav = document.querySelector("header nav");
@@ -9,3 +10,29 @@ window.addEventListener("DOMContentLoaded", (event) => {
         body.classList.toggle("no-scroll");
     });
 });
+
+// CARRUOSEL
+const grande = document.querySelector('.grande')
+const punto = document.querySelectorAll('.punto')
+
+// Recorrer TODOS los punto
+punto.forEach((cadaPunto, i) => {
+    // Asignamos un CLICK a cadaPunto
+    punto[i].addEventListener('click', () => {
+        // Guardar la posición de ese PUNTO
+        let posicion = i
+        // Calculando el espacio que debe DESPLAZARSE el GRANDE
+        let operacion = posicion * -25
+
+        // MOVEMOS el grande
+        grande.style.transform = `translateX(${operacion}%)`
+
+        // Recorremos TODOS los punto
+        punto.forEach((cadaPunto, i) => {
+            // Quitamos la clase ACTIVO a TODOS los punto
+            punto[i].classList.remove('activo')
+        })
+        // Clase activo en el punto que hemos hecho CLICK
+        punto[i].classList.add('activo')
+    })
+})
