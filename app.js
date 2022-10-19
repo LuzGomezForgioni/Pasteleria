ad99fbc6164cf2adfe9030309e838721564a12bc
Luz Gómez Forgioni <luzgomezforgioni@gmail.com>
2022-10-19 18:01:42 -0300

    Probando form

--- app.js
+++ app.js
@@ -36,3 +36,24 @@ punto.forEach((cadaPunto, i) => {
         punto[i].classList.add('activo')
     })
 })
+
+// FORMULARIO CONTACTO
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    if (response.ok){
+        this.reset()
+        alert('Gracias por contactarme!')
+    }
+}
