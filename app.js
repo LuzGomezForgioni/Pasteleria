4bc23e10a62ea7405d42fe0d7f0056f1a7ad0299
Luz Gómez Forgioni <luzgomezforgioni@gmail.com>
2022-10-21 22:00:03 -0300

    Probando seccion productos

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     });
 });
 
-// CARRUOSEL
+// CARUOSEL
 const grande = document.querySelector('.grande')
 const punto = document.querySelectorAll('.punto')
 
@@ -37,7 +37,26 @@ punto.forEach((cadaPunto, i) => {
     })
 })
 
-// FORMULARIO CONTACTO
+// Consumir API en sección PRODUCTOS
+const pedirProductos = async () => {
+	const resp = await fetch('https://mocki.io/v1/ace8da49-4fc4-482a-839e-dfa9fc101fd1')
+	const data = await resp.json()
+
+	data.forEach((producto)=>{
+		const div = document.createElement('div')
+		div.innerHTML = `
+			<img src='${producto.imagen}'/>
+			<h4>${producto.nombreProducto}</h4>
+			<p>$${producto.precio}</p>
+			<button> Comprar </button>
+		`
+		items.append(div)
+	})
+}
+
+pedirProductos()
+
+// FORMULARIO CONTACTO (Enviar a formspree)
 const $form = document.querySelector('#form')
 
 $form.addEventListener('submit', handleSubmit)
@@ -52,8 +71,93 @@ async function handleSubmit(event){
             'Accept': 'application/json'
         }
     })
-    if (response.ok){
-        this.reset()
-        alert('Gracias por contactarme!')
-    }
 }
+
+// VALIDACION DE FORMULARIO
+const form = document.getElementById('form');
+const inputs = document.querySelectorAll('#form input');
+const textarea = document.querySelectorAll('#form textarea');
+
+const expresiones = {
+	apellido: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, // Letras y espacios, pueden llevar acentos.
+	nombre: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, // Letras y espacios, pueden llevar acentos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+    comentarios: /^[a-zA-ZÀ-ÿ\s]{20,300}$/ //Letras y espacios, pueden llevar acentos.
+}
+
+const campos = {
+	apellido: false,
+	nombre: false,
+	correo: false,
+	telefono: false,
+    comentarios: false
+}
+
+const validarform = (e) => {
+	switch (e.target.name) {
+		case "apellido":
+			validarCampo(expresiones.apellido, e.target, 'apellido');
+		break;
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'correo');
+		break;
+		case "telefono":
+			validarCampo(expresiones.telefono, e.target, 'telefono');
+		break;
+        case "comentarios":
+			validarCampo(expresiones.comentarios, e.target, 'comentarios');
+		break;
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('form__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('form__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .form__input-error`).classList.remove('form__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('form__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('form__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .form__input-error`).classList.add('form__input-error-activo');
+		campos[campo] = false;
+	}
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarform);
+	input.addEventListener('blur', validarform);
+});
+
+textarea.forEach((textarea) => {
+	textarea.addEventListener('keyup', validarform);
+	textarea.addEventListener('blur', validarform);
+});
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const terminos = document.getElementById('terminos');
+	if(campos.apellido && campos.nombre && campos.correo && campos.telefono && campos.comentarios && terminos.checked ){
+		form.reset();
+
+		document.getElementById('form__mensaje-exito').classList.add('form__mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('form__mensaje-exito').classList.remove('form__mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('.form__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('form__grupo-correcto');
+		});
+	} else {
+		document.getElementById('form__mensaje').classList.add('form__mensaje-activo');
+	}
+});
